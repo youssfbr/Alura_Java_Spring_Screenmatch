cd4e68e4c2b96fff6c3685d75d707a026c46b759
Alisson Youssf <youssfbr@gmail.com>
2024-10-28 13:14:10 -0300

    Initial commit

---
+++ src/main/java/com/github/com/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,13 @@
+package com.github.com.screenmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+}
