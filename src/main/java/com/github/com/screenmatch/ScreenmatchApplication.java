abfe24fde213e69b02c4712770bd9de56e248a2e
Alisson Youssf <youssfbr@gmail.com>
2024-11-11 21:48:28 -0300

    Transformando o projeto em api web, starter web

--- src/main/java/com/github/com/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/github/com/screenmatch/ScreenmatchApplication.java
@@ -1,27 +1,11 @@
 package com.github.com.screenmatch;
 
-import com.github.com.screenmatch.principal.Principal;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
-
-	private final Principal principal;
-
-    public ScreenmatchApplication(Principal principal) {
-        this.principal = principal;
-    }
-
+public class ScreenmatchApplication {
     public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		principal.exibeMenu();
-
 	}
 }
---
+++ src/main/java/com/github/com/screenmatch/ScreenmatchApplicationSemWeb.java
@@ -0,0 +1,27 @@
+package com.github.com.screenmatch;
+
+import com.github.com.screenmatch.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class ScreenmatchApplicationSemWeb implements CommandLineRunner {
+//
+//	private final Principal principal;
+//
+//    public ScreenmatchApplicationSemWeb(Principal principal) {
+//        this.principal = principal;
+//    }
+//
+//    public static void main(String[] args) {
+//		SpringApplication.run(ScreenmatchApplicationSemWeb.class, args);
+//	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//
+//		principal.exibeMenu();
+//
+//	}
+//}
